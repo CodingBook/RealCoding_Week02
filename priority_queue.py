5162726e3268b74f04f63e777a2093cee711924a
BookShelf <97891921+CodingBook@users.noreply.github.com>
2024-03-11 17:49:18 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -11,7 +11,7 @@ class PriorityQueue:
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
       item = self.heap.pop()[1]
-      self._sift_down(len(self.heap) - 1)
+      self._sift_down(0)
       return item
     elif len(self.heap) == 1:
       item = self.heap.pop()[1]
@@ -19,6 +19,7 @@ class PriorityQueue:
     else:
       return None
 
+  # 잎에서 올라가며 비교하는 함수, 우선순위가 높을 수록(숫자가 작을수록) 루트로, 최소힙?
   def _sift_up(self, index):
     while index > 0:
       parent_index = (index - 1) // 2
@@ -28,18 +29,19 @@ class PriorityQueue:
       else:
         break
 
+  # 루트에 있는걸 알맞게 배치하는 함수, 
   def _sift_down(self, index):
     while True:
       left_child_index = 2 * index + 1
       right_child_index = 2 * index + 2
-      smallest = index
+      smallest = index # 우선순위가 가장 낮은 녀석(== 숫자가 높은 녀석)
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest][0]:
+        self.heap[left_child_index][0] < self.heap[smallest][0]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest][0]:
+        self.heap[right_child_index][0] < self.heap[smallest][0]:
         smallest = right_child_index
 
       if smallest != index:
