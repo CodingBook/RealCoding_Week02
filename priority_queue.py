282fe2822188c66600fd33f6ab3f003a35b71a3b
BookShelf <97891921+CodingBook@users.noreply.github.com>
2024-03-11 18:00:57 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -1,12 +1,17 @@
 class PriorityQueue:
+
+  # 리스트를 이용해 힙 생성, 
+  # 우선순위가 높을 수록(==priority 값이 낮을 수록) 루트로 오게 만들기 (최소힙)
   def __init__(self):
     self.heap = []
 
+  # 맨 뒤에 삽입 후, _sift_up으로 제자리 찾기
   def push(self, item, priority):
     entry = (priority, item)
     self.heap.append(entry)
     self._sift_up(len(self.heap) - 1)
 
+  # 기존 루트를 맨 뒤로 보내 팝하고, _sift_down으로 알맞게 다시 위치 시키기
   def pop(self):
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
@@ -19,7 +24,7 @@ class PriorityQueue:
     else:
       return None
 
-  # 잎에서 올라가며 비교하는 함수, 우선순위가 높을 수록(숫자가 작을수록) 루트로, 최소힙?
+  # 잎에서 올라가며 비교하는 함수, 부모가 우선순위가 낮으면, 스왑
   def _sift_up(self, index):
     while index > 0:
       parent_index = (index - 1) // 2
@@ -29,7 +34,7 @@ class PriorityQueue:
       else:
         break
 
-  # 루트에 있는걸 알맞게 배치하는 함수
+  # 루트에 있는걸 알맞게 배치하는 함수, 
   def _sift_down(self, index):
     while True:
       left_child_index = 2 * index + 1
